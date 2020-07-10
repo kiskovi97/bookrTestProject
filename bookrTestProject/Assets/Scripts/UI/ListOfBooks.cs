c33c8360527fa578d70e731606925a90f92ab6e2
kiskovi97 <kiskovi97@gmail.com>
2020-07-10 20:16:38 +0200

    Grid Layout

--- bookrTestProject/Assets/Scripts/UI/ListOfBooks.cs
+++ bookrTestProject/Assets/Scripts/UI/ListOfBooks.cs
@@ -9,8 +9,6 @@ public class ListOfBooks : MonoBehaviour
 {
     public List<BookUI> bookUIList = new List<BookUI>();
 
-    public GameObject Line;
-
     public GameObject bookUIPrefab;
 
     public int LineCount = 5;
@@ -19,19 +17,12 @@ public class ListOfBooks : MonoBehaviour
     {
         DestroyAllChildren();
 
-        int count = 0;
-        var currentLine = Instantiate(Line, transform);
         foreach (var book in books)
         {
-            if (count != 0 && count % LineCount == 0)
-            {
-                currentLine = Instantiate(Line, transform);
-            }
-            var obj = Instantiate(bookUIPrefab, currentLine.transform);
+            var obj = Instantiate(bookUIPrefab, transform);
             var bookUI = obj.GetComponent<BookUI>();
             bookUI.SetBook(book);
             bookUIList.Add(bookUI);
-            count++;
         }
     }
 
